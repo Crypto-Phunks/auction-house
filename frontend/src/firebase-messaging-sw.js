569b9eda9674edba8f584593ec4e3ced4503b02b
chopper <chopper69daddy@gmail.com>
2023-09-05 17:23:34 -0600

    disabled sw

--- frontend/src/firebase-messaging-sw.js
+++ frontend/src/firebase-messaging-sw.js
@@ -1,38 +1,38 @@
-// Import the Firebase initialization and Messaging SDK
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+// // Import the Firebase initialization and Messaging SDK
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAcwtSryk3XfnoabYNO5uIcjbXIaWkoPjI',
-  authDomain: 'cryptophunks-auction-house.firebaseapp.com',
-  projectId: 'cryptophunks-auction-house',
-  storageBucket: 'cryptophunks-auction-house.appspot.com',
-  messagingSenderId: '356196458803',
-  appId: '1:356196458803:web:9d9de545fd5c3db0a29e46',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAcwtSryk3XfnoabYNO5uIcjbXIaWkoPjI',
+//   authDomain: 'cryptophunks-auction-house.firebaseapp.com',
+//   projectId: 'cryptophunks-auction-house',
+//   storageBucket: 'cryptophunks-auction-house.appspot.com',
+//   messagingSenderId: '356196458803',
+//   appId: '1:356196458803:web:9d9de545fd5c3db0a29e46',
+// };
 
-// Initialize Firebase with your configuration
-firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase with your configuration
+// firebase.initializeApp(firebaseConfig);
 
-// Retrieve an instance of Firebase Messaging
-const messaging = firebase.messaging();
+// // Retrieve an instance of Firebase Messaging
+// const messaging = firebase.messaging();
 
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.onBackgroundMessage` handler.
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon,
-  };
+// // Handle incoming messages. Called when:
+// // - a message is received while the app has focus
+// // - the user clicks on an app notification created by a service worker
+// //   `messaging.onBackgroundMessage` handler.
+// messaging.onBackgroundMessage((payload) => {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   // Customize notification here
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: payload.notification.icon,
+//   };
 
-  // Show the notification
-  self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
-});
+//   // Show the notification
+//   self.registration.showNotification(
+//     notificationTitle,
+//     notificationOptions
+//   );
+// });
