7ea0daeb9d3c510c827f2bc3432763f9e794515d
chopper <chopper69daddy@gmail.com>
2023-09-05 11:15:08 -0600

    prod tests

--- bot/src/services/web3.service.ts
+++ bot/src/services/web3.service.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
-import auctionABI from '../abi/AuctionHouseABI.json';
-import punkDataABI from '../abi/PunkData.json';
-
 import { createPublicClient, formatUnits, http } from 'viem'
 import { goerli, mainnet } from 'viem/chains'
 
+import { auctionHouse } from 'src/abi/AuctionHouseABI';
+import { punkData } from 'src/abi/PunkData';
+
 import { Auction, AuctionLog } from 'src/interfaces/auction.interface';
 
-import dotenv from 'dotenv';
 import { BehaviorSubject } from 'rxjs';
+
+import dotenv from 'dotenv';
 dotenv.config();
-
-const auctionContractAddress = process.env.AUCTION_CONTRACT_ADDRESS;
-const punkDataAddress = process.env.PUNK_DATA_ADDRESS;
-
-const client = createPublicClient({
-  chain: goerli,
-  transport: http(process.env.RPC_URL),
-});
 
 @Injectable()
 export class Web3Service {
@@ -29,27 +22,32 @@ export class Web3Service {
   private auctionCreated = new BehaviorSubject<AuctionLog | null>(null);
   auctionCreated$ = this.auctionCreated.asObservable();
 
+  client = createPublicClient({
+    chain: goerli,
+    transport: http(process.env.RPC_URL),
+  });
+
   constructor() {
 
-    client.watchContractEvent({
-      address: auctionContractAddress as `0x${string}`,
-      abi: auctionABI,
+    this.client.watchContractEvent({
+      address: auctionHouse.address as `0x${string}`,
+      abi: auctionHouse.abi,
       eventName: 'AuctionCreated',
       onLogs: (logs: any[] /* wtf viem */) => this.auctionCreated.next(logs[0] as AuctionLog)
     });
 
-    client.watchContractEvent({
-      address: auctionContractAddress as `0x${string}`,
-      abi: auctionABI,
+    this.client.watchContractEvent({
+      address: auctionHouse.address as `0x${string}`,
+      abi: auctionHouse.abi,
       eventName: 'AuctionBid',
       onLogs: (logs: any[] /* wtf viem */) => this.auctionBid.next(logs[0] as AuctionLog)
     });
   }
 
   async getCurrentAuction(): Promise<Auction> {
-    const res = await client.readContract({
-      address: auctionContractAddress as `0x${string}`,
-      abi: auctionABI,
+    const res = await this.client.readContract({
+      address: auctionHouse.address as `0x${string}`,
+      abi: auctionHouse.abi,
       functionName: 'auction',
       args: [],
     });
@@ -66,29 +64,29 @@ export class Web3Service {
   }
 
   async getPunkImage(phunkId: string): Promise<string> {
-    return await client.readContract({
-      address: punkDataAddress as `0x${string}`,
-      abi: punkDataABI,
+    return await this.client.readContract({
+      address: punkData.address as `0x${string}`,
+      abi: punkData.abi,
       functionName: 'punkImage',
       args: [phunkId],
     }) as string;
   }
 
   async getPunkAttributes(phunkId: string): Promise<string> {
-    return await client.readContract({
-      address: punkDataAddress as `0x${string}`,
-      abi: punkDataABI,
+    return await this.client.readContract({
+      address: punkData.address as `0x${string}`,
+      abi: punkData.abi,
       functionName: 'punkAttributes',
       args: [phunkId],
     }) as string;
   }
 
   async getTransactionReceipt(hash: `0x${string}`) {
-    return await client.getTransactionReceipt({ hash });
+    return await this.client.getTransactionReceipt({ hash });
   }
 
   async getEnsFromAddress(address: `0x${string}`): Promise<string> {
-    return await client.getEnsName({ address });
+    return await this.client.getEnsName({ address });
   }
 
   weiToEth(wei: bigint): string {
